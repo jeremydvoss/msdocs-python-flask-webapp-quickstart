20c1ebdfb6a2e001dbf0a4670d84d337a07b1f37
jerevoss <jerevoss@gmail.com>
2022-06-23 17:29:04 -0700

    Added OpenCensus manual instrumentation

--- app.py
+++ app.py
@@ -1,29 +1,44 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 app = Flask(__name__)
+import logging
+from opencensus.ext.azure.log_exporter import AzureLogHandler
 
+logger = logging.getLogger(__name__)
+
+# TODO: replace the all-zero GUID with your instrumentation key.
+logger.addHandler(AzureLogHandler(
+    connection_string='InstrumentationKey=f6185f23-a25a-4f83-994b-acc8acb546bf')
+)
+# You can also instantiate the exporter directly if you have the environment variable
+# `APPLICATIONINSIGHTS_CONNECTION_STRING` configured
+# logger.addHandler(AzureLogHandler())
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    logging.log("JEREMY: index")
+    print('Request for index page received')
+    return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
+    logging.log("JEREMY: favicon")
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
+    logging.log("JEREMY: hello")
+    name = request.form.get('name')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    if name:
+        print('Request for hello page received with name=%s' % name)
+        return render_template('hello.html', name = name)
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
-   app.run()
+    logging.log("JEREMY: run")
+    app.run()
