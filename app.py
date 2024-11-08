1ed0afd7b528e83ed59680a95799c3bc3d1a13b1
jerevoss <jerevoss@gmail.com>
2024-11-07 16:01:05 -0800

    simple test app for yutang

--- app.py
+++ app.py
@@ -1,29 +1,90 @@
-from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+# Add to test manual instrumentation or backoff scenarios
+# # from azure.monitor.opentelemetry import configure_azure_monitor
+# configure_azure_monitor()
+
+from flask import Flask, url_for
 app = Flask(__name__)
+import logging
+import requests
+
+
+# Set up logger
+logger = logging.getLogger(__name__)
 
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    return 'Flask Test app\nVisit /logs and /traces.'
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+# Logs -> traces
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+@app.route('/logs/info')
+def logs_info():
+    message = 'Test app info log'
+    logger.info(message)
+    return message
+
+
+@app.route('/logs/warning')
+def logs_warning():
+    message = 'Test app warning log'
+    logger.warning(message)
+    return message
+
+
+@app.route('/logs/error')
+def logs_error():
+    message = 'Test app error log'
+    logger.error(message)
+    return message
+
+
+@app.route('/logs/exception')
+def logs_exception():
+    message = 'Test app exception log'
+    logger.exception(message)
+    return message
+
+
+@app.route('/logs')
+def logs():
+    logs_info()
+    logs_warning()
+    logs_error()
+    logs_exception()
+    return "Test app logs"
+
+
+# Traces -> requests
+
+@app.route('/traces/requests')
+def traces_requests():
+    return "Test app request"
+
+
+@app.route('/traces/exception_requests')
+def traces_exception_requests():
+    return "Test app exception request", 500
+
+
+@app.route('/traces/dependencies')
+def traces_dependencies():
+    requests.get('https://learn.microsoft.com/en-us/azure/azure-monitor/app/opentelemetry-enable?tabs=python')
+    return "Test app dependency"
+
+@app.route('/traces')
+def traces():
+    for endpoint in ('traces_requests', 'traces_exception_requests', 'traces_dependencies'):
+        try:
+            requests.get(url_for(endpoint))
+        except:
+            pass
+    return 'Test app traces'
 
 
 if __name__ == '__main__':
-   app.run()
+    logger.info("App Running")
+    logger.warning("App Running Warning")
+    requests.get('https://learn.microsoft.com/en-us/azure/azure-monitor/app/opentelemetry-enable?tabs=python')
+    app.run(port=8082)
